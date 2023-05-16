96499309045c8c9b6ef7d9943898206eecd4571c
Darlan Gabriel Leite <43675323+DarlanSchwartz@users.noreply.github.com>
2023-05-16 10:14:01 -0300

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import styled from 'styled-components'
+import GlobalStyle from './globalStyles';
+
+export default function App() {
+
+  return (
+    <>
+        <GlobalStyle/>
+        
+    </>
+  )
+}
