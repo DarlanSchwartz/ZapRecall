85245456480af801e5252b8cf3d206cdf5e3a5d9
Darlan Gabriel Leite <43675323+DarlanSchwartz@users.noreply.github.com>
2023-05-18 19:06:51 -0300

    data test 2

--- src/Recall.jsx
+++ src/Recall.jsx
@@ -119,7 +119,7 @@ export default function Recall(props)
     
     return(
         <RecalComponent data-test="flashcard" textColor = {tColor} textDecoration = {tDecoration} open = {props.open} turned = {props.turned} >
-            {props.open ? <p data-test="flashcard-text">{props.turned ? props.answer : props.question}</p> : <p>Pergunta {props.index + 1}</p>}
+            {props.open ? <p data-test="flashcard-text">{props.turned ? props.answer : props.question}</p> : <p data-test="flashcard-text">Pergunta {props.index + 1}</p>}
             {!props.open && <img data-test={dataTestIcon} onClick={() =>props.setRecall(props.index,true)} className="open-btn" src={icon}/>}
             {(props.open && !props.turned) && <img data-test="turn-btn" onClick={() => props.setTurned(props.index,true)} className="turn-btn" src={TurnIcon}/>}
             {props.turned && <div className="choices"><button data-test="no-btn" onClick={()=> props.setState(props.index,3)} className="didnt-remember-btn">Não Lembrei</button><button  data-test="partial-btn" onClick={()=> props.setState(props.index,2)} className="almost-remember-btn">Quase não Lembrei</button><button  data-test="zap-btn"  onClick={()=> props.setState(props.index,1)}className="zap-btn">Zap!</button></div>}
